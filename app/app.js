c173466a369606b8253051409339b3ad6e298e75
suryateja-207 <grandhisurya@gmail.com>
2016-06-24 03:21:40 +0530

    header added

--- app/app.js
+++ app/app.js
@@ -9,6 +9,6 @@ angular.module('myApp', [
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
-
+    
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
