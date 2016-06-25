caa59be6906fc283e044f9c3d89a7d54bfcc572a
suryateja-207 <grandhisurya@gmail.com>
2016-06-26 03:16:17 +0530

    Multi-bar chart for price and month for both types cash and credit were included.Pie chart for expenses and name included.

--- app/app.js
+++ app/app.js
@@ -6,36 +6,40 @@ angular.module("dekorateApp", ['nvd3','ngMaterial']).controller("expensesControl
         { 'name':'Food',
             'type': 'cash',
             'price': '500',
-            'date' : '2016-04-11', 'color':'red'
+            'date' : '2016-04-11'
         },
         { 'name':'Laptop Repair',
             'type': 'credit',
             'price': '100',
-            'date' : '2016-04-11',
-            'color':'green'},
+            'date' : '2016-04-11'
+        },
         { 'name':'Laptop Repair',
             'type': 'credit',
             'price': '100',
-            'date' : '2016-04-11',
-            'color':'green'},
+            'date' : '2016-04-11'
+            },
         { 'name':'Laptop Repair',
             'type': 'cash',
             'price': '100',
-            'date' : '2016-03-11',
-            'color':'green'},
+            'date' : '2016-03-11'
+        }
     ];
     console.log($scope.expensesTable);
     // $scope.expensesTable = [];
     $scope.addRow = function(){
-        if ($scope.name !== null && $scope.type !== null && $scope.price !==null && $scope.date !== null) {
+        if ($scope.name != null && $scope.type != null && $scope.price != null && $scope.date != null) {
             $scope.expensesTable.push({
                 'name': $scope.name,
                 'type': $scope.type,
                 'price': $scope.price,
                 'date': $scope.date
             });
+            var frm = document.getElementsByName('expense-form')[0];
+            // Submit
+            frm.reset();  // Reset
         }
         console.log($scope.expensesTable);
+        $scope.expensesTable.sort(custom_sort);
         s();
     };
     $scope.removeRow = function(index){
@@ -77,14 +81,17 @@ angular.module("dekorateApp", ['nvd3','ngMaterial']).controller("expensesControl
             duration: 500,
             stacked: true,
             xAxis: {
-                axisLabel: 'Time (ms)',
+                axisLabel: 'Month-Year',
                 showMaxMin: false,
                 tickFormat: function(d){
-                    return d3.format(',f')(d);
+                    console.log(d, "vinodh");
+                    console.log($scope.dateData[d].key,"sury");
+                    return $scope.dateData[d].key;
+                    //return d3.format(',f')(d);
                 }
             },
             yAxis: {
-                axisLabel: 'Y Axis',
+                axisLabel: 'Price',
                 axisLabelDistance: -20,
                 tickFormat: function(d){
                     return d3.format(',.1f')(d);
@@ -92,7 +99,7 @@ angular.module("dekorateApp", ['nvd3','ngMaterial']).controller("expensesControl
             }
         }
     };
-
+    $scope.axisValue = '';
     // $scope.nest = d3.nest()
     //     .key(function (d) {
     //         return d.type;
@@ -105,78 +112,68 @@ angular.module("dekorateApp", ['nvd3','ngMaterial']).controller("expensesControl
     //         key: 'Stream' + i,
     //         values: data
     //     };});
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-
-    function generateData() {
-        var nest = d3.nest()
-            .key(function (d) {
-                return months[(new Date(d.date)).getUTCMonth()] + '-' + new Date(d.date).getUTCFullYear();
-            }).entries($scope.expensesTable);
-        return nest;
-    };
-    $scope.dateData = generateData();
-    console.log($scope.dateData ,"datedata");
-    $scope.typeDate = d3.nest()
-        .key(function (d) {
-            return d.type;
-        })
-        .entries($scope.expensesTable);
-    console.log($scope.typeDate);
-    $scope.finalData = [];
-    var element = {};
-    element.key = 'cash';
-    element.values = [];
-    $scope.finalData.push(element);
-    var element = {};
-    element.key = 'credit';
-    element.values = [];
-    $scope.finalData.push(element);
-    for(var i = 0; i < $scope.dateData.length ; i++)
-    {
-        // var element = {};
-        // element.key = 'cash';
-        // element.values = [];
-        // $scope.finalData.push('cash');
-        // $scope.finalData.push('credit');
-        // $scope.finalData['cash'] = {};
-        // $scope.finalData['credit'] = {};
-        // console.log($scope.finalData);
-        // $scope.finalData['cash'][$scope.dateData[0].key] = 0;
-        // $scope.finalData['credit'][$scope.dateData[0].key] = 0;
-        var cash = 0;
-        var credit = 0;
-        console.log($scope.dateData[i].values.length, "rere");
-        for (var j = 0; j<$scope.dateData[i].values.length;j++)
-        {
-            if($scope.dateData[i].values[j].type == "cash")
-            {
-                cash+= parseInt($scope.dateData[i].values[j].price);
-            }
-            else
-            {
-                credit+= parseInt($scope.dateData[i].values[j].price);
-            }
-        }
-        var element = {};
-        element.date =  $scope.dateData[i].key;
-        element.price = cash;
-        $scope.finalData[0].values.push(element);
-        var element = {};
-        element.date =  $scope.dateData[i].key;
-        element.price = credit;
-        $scope.finalData[1].values.push(element);
-
-
+    //console.log(finalData, "finalData");
+    function custom_sort(a, b) {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
     }
-    console.log($scope.finalData, "finalData");
+
+    $scope.expensesTable.sort(custom_sort);
     var s = function () {
-        var nest = d3.nest()
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+        function generateData() {
+            var nest = d3.nest()
+                .key(function (d) {
+                    return months[(new Date(d.date)).getUTCMonth()] + '-' + new Date(d.date).getUTCFullYear();
+                }).entries($scope.expensesTable);
+            return nest;
+        };
+        $scope.dateData = generateData();
+        console.log($scope.dateData ,"datedata");
+        $scope.typeDate = d3.nest()
             .key(function (d) {
                 return d.type;
             })
             .entries($scope.expensesTable);
+        console.log($scope.typeDate);
+        var finalData = [];
+        var element = {};
+        element.key = 'cash';
+        element.values = [];
+        finalData.push(element);
+        var element = {};
+        element.key = 'credit';
+        element.values = [];
+        finalData.push(element);
+        for(var i = 0; i < $scope.dateData.length ; i++)
+        {
+            var cash = 0;
+            var credit = 0;
+            console.log($scope.dateData[i].values.length, "rere");
+            for (var j = 0; j<$scope.dateData[i].values.length;j++)
+            {
+                if($scope.dateData[i].values[j].type == "cash")
+                {
+                    cash+= parseInt($scope.dateData[i].values[j].price);
+                }
+                else
+                {
+                    credit+= parseInt($scope.dateData[i].values[j].price);
+                }
+            }
+            var element = {};
+            element.date =  $scope.dateData[i].key;
+            element.price = cash;
+            finalData[0].values.push(element);
+            var element = {};
+            element.date =  $scope.dateData[i].key;
+            element.price = credit;
+            finalData[1].values.push(element);
+        }
+        var nest = finalData;
         console.log(nest, "sdds");
         var k = 0;
+        var type = ['cash','credit'];
         $scope.bardata = d3.range(2).map(function () {
             var a = nest[k];
             console.log(nest[k]);
@@ -184,11 +181,11 @@ angular.module("dekorateApp", ['nvd3','ngMaterial']).controller("expensesControl
             return a.values.map(stream_index);
         }).map(function (data, i) {
             return {
-                key: 'Stream' + i,
+                key: type[i],
                 values: data
             };
         });
-        // console.log($scope.bardata);
+        console.log($scope.bardata);
     };
     // $scope.sdata = generateData();
     // /* Random Data Generator (took from nvd3.org) */
